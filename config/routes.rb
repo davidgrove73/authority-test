4cd8952d36e3bd55bcca9b6e1bf1018e383c0493
David Grove Jørgensen <dgj@kb.dk>
2015-11-19 15:07:19 +0100

    added authorities to routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root :to => "catalog#index"
   blacklight_for :catalog
   devise_for :users
+  mount Authority::Engine => "/authority"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
